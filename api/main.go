20bf840d050c59ec9783eb518572d097dc82f55c
Aditya Jha <mail@adijha.com>
2022-03-05 14:10:12 +0530

    converted-to-fiber

--- api/main.go
+++ api/main.go
@@ -1,13 +1,13 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
 	})
-	r.Run(":5000")
+
+	app.Listen(":80")
 }
